540a919d19a167f3ea14cc924cde6e7e444acefd
khushwant04 <khushwantsanwalot@outlook.com>
2024-07-11 18:55:34 +0000

    commit

--- data/index.ts
+++ data/index.ts
@@ -179,29 +179,29 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Machine Learning Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Machine Learning",
+    desc: "Created many advance applications using PyTorch.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
     title: "Web Devlopement",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    desc: "Designed and developed responsive websites using React.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
     title: "Freelance AI Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    desc: "Worked on many opensource projects.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
   },
   {
     id: 4,
     title: "Lead AI Devloper",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    desc: "Developed and maintained user-facing features using modern AI technologies.",
     className: "md:col-span-2",
     thumbnail: "/exp4.svg",
   },
